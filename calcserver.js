1fa027551b2bdd79ee726386328fa758c5976a90
Kallol Chatterjee <kallol.chatterjee90@gmail.com>
2016-03-24 15:20:32 -0700

    Task3 Completed.... Findtotal needs to be implemented next based on command

--- calcserver.js
+++ calcserver.js
@@ -1,13 +1,48 @@
 // simple socket server
 var net = require('net');
-net.createServer(function (sock) {
+var EventEmitter = require('events').EventEmitter;
+var total = 0;
+var lumberjack = new EventEmitter();
+var calculator = new EventEmitter();
+var clients = {};
+var clientId,cmd,value;
+function display(lumberDisplay) {
+    console.log(lumberDisplay);
+}
+lumberjack.on('lumberjack', display);
+
+
+
+function checkClient(clientId,cmd, value, sock){
+  console.log('ClientID : ' + clientId);
+  if (clientId === "ASU") {
+    setTimeout(function () {
+	findTotal(cmd, value, sock, clientId);
+    }, 30000);
+  } else if (clientId === "UA") {
+    setImmediate(function(){
+	findTotal(cmd, value, sock, clientId);
+    });
+
+  } else if (clientId === "NAU") {
+    lumberjack.emit('lumberjack', 'I saw a lumberjack!');
+	findTotal(cmd, value, sock, clientId);
+  }
+}
+
+function findTotal(cmd,value,sock,clientId){
+
+}
+
+var server = net.createServer(function (sock) {
     console.log("Incoming connection accepted");
     sock.on('data', function (d) {
-        console.log("Received from client: " + d);
-        sock.write(d+d, function() {
-        	console.log("Finished response to client");
-        })
+      var dataset = d.toString().split(" ");
+      clientId = dataset[0];
+      cmd = dataset[1];
+      value =dataset[2];
+      checkClient(clientId,cmd, value,sock);
     }).on('error', function (e) {
-	console.log("Some kind of server error");
+	console.log("Some kind of server error" + e);
     });
 }).listen(3000);
